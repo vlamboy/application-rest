9ac73f4fc113c0cdc4bcae960c2835bd8669bace
vlamboy <vivienne.lamboy@student.htw-berlin.de>
2023-01-04 12:38:34 +0100

    initial commit

---
+++ src/main/java/de/example/applicationrest/ApplicationRestApplication.java
@@ -0,0 +1,13 @@
+package de.example.applicationrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationRestApplication.class, args);
+	}
+
+}
